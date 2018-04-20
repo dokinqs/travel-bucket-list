ea1dd6b7404c3b86f200fe978281c523d7d6b17c
Karen Zhao <karen.zhao18@gmail.com>
2018-04-20 10:32:04 -0400

    locationsViewController - render views

--- controllers/locationsViewController.js
+++ controllers/locationsViewController.js
@@ -1,19 +1,28 @@
 function sendLocations(req, res) {
-  console.log('I send successful responses');
-  res.json({
-    status: 'ok',
+  // console.log('I send successful responses');
+  // res.json({
+  //   status: 'ok',
+
+  // points to file "index.ejs" inside "views/locations"
+  res.render('locations/index', {
     locations: res.locals.locations
   });
 }
 
 function sendOneLocation(req, res) {
-  res.json({
-    status: 'ok',
+  // res.json({
+  //   status: 'ok',
+
+  // "location.ejs" view makes use of the same partial single_location.ejs
++  // for rendering one location as the index view used to render each location in the list
++  // of locations that were passed in to locations/index
+  res.render('locations/location', {
     location: res.locals.location
   });
 }
 
 function sendCreateLocation(req, res) {
+  // or write "locations/new" view and call "res.render('locations/new')"
   res.json({
     status: 'ok',
     location: res.locals.newLocation
