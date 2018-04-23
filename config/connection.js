064557800e4c3673506b7a50ee22845e9d43cbbc
Karen Zhao <karen.zhao18@gmail.com>
2018-04-23 16:05:24 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,10 @@ const pgp = require('pg-promise')();
 // require obj, connect to db
 // check export properties
 const config = require('./dbConfig');
+
 // pass to pgp to connect to db
-const db = pgp(config);
+const db = pgp(process.env.DATABASE_URL || config);
+
 // confirm configuration
 console.log(db);
 
